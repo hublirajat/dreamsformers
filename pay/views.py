eabcdcd333fb5a943c73aeee7c3dbed6111a866a
Cameron Morris <cameron.morris@amadeus.com>
2016-04-12 13:52:01 -0400

    test ajax

--- pay/views.py
+++ pay/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-
+@csrf_exempt
 def fbcallback(request):
     if request.method == 'POST':
         print 'Raw Data: "%s"' % request.body
