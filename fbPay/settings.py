dd6aceea5189c48b8d18fbb3e8602f165e672434
Cameron Morris <cameron.morris@amadeus.com>
2016-04-12 14:37:59 -0400

    db?

--- fbPay/settings.py
+++ fbPay/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
