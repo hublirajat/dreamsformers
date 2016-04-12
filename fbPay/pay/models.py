540f852567214ff61f61d7c1509d72f0836da00f
Cameron Morris <cameron.morris@amadeus.com>
2016-04-12 11:57:05 -0400

    lots of stuff

--- fbPay/pay/models.py
+++ fbPay/pay/models.py
@@ -15,7 +15,7 @@ class Booking(models.Model):
         ticketNumber = models.CharField(max_length=200)
         origin = models.CharField(max_length=100)
 	destination = models.CharField(max_length=100)
-	dateOfBooking = models.DateTimeField(uto_now_add=True)
+	dateOfBooking = models.DateTimeField(auto_now_add=True)
 	dateOfTravel = models.DateTimeField()
 
 
@@ -25,5 +25,3 @@ class Payment(models.Model):
 	amount = models.CharField(max_length=200)
 	currency =  models.CharField(max_length=200)
 	approvalCode = models.CharField(max_length=200)
-
-
