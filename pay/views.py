987e6d7565d1fb8b68557f372cb338ecc10a4382
Cameron Morris <cameron.morris@amadeus.com>
2016-04-12 15:19:49 -0400

    userprofile in admin

--- pay/views.py
+++ pay/views.py
@@ -15,6 +15,7 @@ def fbcallback(request):
         print 'Json Data: "%s"' % json_data
         newuser = UserProfile()
         newuser.fbId = json_data["id"]
+        print 'FBID:',json_data["id"]
         newuser.firstName = json_data["name"].split(' ')[0]
         newuser.lastName = json_data["name"].split(' ')[1]
         newuser.save()
