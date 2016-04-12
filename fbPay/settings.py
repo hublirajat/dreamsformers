4f3e4fadd56a784d807c0eb9075bc6de2dd78932
Cameron Morris <cameron.morris@amadeus.com>
2016-04-12 14:28:48 -0400

    db?

--- fbPay/settings.py
+++ fbPay/settings.py
@@ -115,6 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
