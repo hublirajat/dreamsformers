207b296af4f7605232227fdf3c35354b377e1cb8
Applicative Privilegied user <auser@mucsmtp3.amadeus.net>
2016-04-13 13:35:20 +0000

    Forms

--- pay/views.py
+++ pay/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from .models import UserProfile
+from .models import Booking
+from .models import Payment
+from .forms import BookingCreationForm
+import random
 from random import randint
 import string
 
@@ -9,7 +13,8 @@ import string
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+	form = BookingCreationForm()
+	return render(request, 'index.html', {'form': form})
 
 @csrf_exempt
 def fbcallback(request):
@@ -25,19 +30,34 @@ def fbcallback(request):
 
     return HttpResponse("OK");
 
+@csrf_exempt
 def createBooking(request):
         if request.method == 'POST':
-                form = EventCreationForm(request.POST)
+		# form is the object for class BookingCreationForm defined in Forms.py
+		form = BookingCreationForm(request.POST)
                 if form.is_valid():
-
+                        origin = form.cleaned_data['origin']
+                        destination = form.cleaned_data['destination']
+                        traveldate = form.cleaned_data['traveldate']
+			
+			# Randomly generate Booking Reference 6 ASCII
 			bookingRef = id_generator()
-		        ticketNumber = random_with_N_digits(9)
-		        origin = form.cleaned_data['fromcity']
-			destination = form.cleaned_data['tocity']
-			dateOfTravel = form.cleaned_data['datepickerstart']
+
+			# Randomly generate Ticket Number 9 digit
+			ticketNumber = random_with_N_digits(9)
+			print bookingRef
 
 			# Creating a Booking
-			booking = Booking.objects.create(bookingRef = bookingRef, ticketNumber = ticketNumber, origin = origin, destination = destination, dateOfTravel = dateOfTravel)
+			booking = Booking.objects.create(bookingRef = bookingRef, ticketNumber = ticketNumber, origin = origin, destination = destination, dateOfTravel = traveldate)
+			booking.save()
+
+			# Creating a Payment for the same Booking			
+			amount = random_with_N_digits(4)
+			currency = "USD"
+			paymentId = random_with_N_digits(8)
+			payment = Payment.objects.create(bookingRef = booking, amount = amount, currency = currency, paymentId = paymentId)
+			payment.save()
+	return HttpResponse("OK");
 
 def random_with_N_digits(n):
     range_start = 10**(n-1)
